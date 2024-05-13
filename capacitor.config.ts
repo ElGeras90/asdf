6ed881ad3794719330e0229b7c444f843c220725
Juan Gerardo Garcia Salazar <jackiejggs@gmail.com>
2024-05-13 21:11:10 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'asdf',
+  webDir: 'www'
+};
+
+export default config;
